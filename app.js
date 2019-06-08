dcf4faea35a50895d9421995c563ae49bc751347
imnotasophist <romandub25@gmail.com>
2019-06-08 20:14:51 +0400

    jquery!!!

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 $(function() {
+    // Fixed Header
     let header = $("#header");
     let intro = $("#intro");
-    let introH;
-    let scrollPos = $(window).scrollTop()
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
 
-    $(window).on("scroll load resize", function() {
+    checkScroll(scrollPos, introH);
+
+    $(window).on("scroll resize", function() {
         introH = intro.innerHeight();
         scrollPos = $(this).scrollTop();
 
+        checkScroll(scrollPos, introH);
+    });
+    function checkScroll(scrollPos, introH) {
         if (scrollPos > introH) {
             header.addClass("fixed");
         } else {
             header.removeClass("fixed");
         }
+    }
+    // Smooth Scroll
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+        
+        let elementId = $(this).data('scroll');
+        let elementOffset = $(elementId).offset().top;
+
+        $("html, body").animate({
+            scrollTop: elementOffset - 70
+        }, 700);
     });
 });
